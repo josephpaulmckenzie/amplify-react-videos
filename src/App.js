42136fab80352c3e680eb538edd0787da0ae24eb
Joseph Mckenzi <josephmckenzieaz@yahoo.com>
2019-06-12 00:44:16 -0400

    first commit of base code, cognito to authenicate,uploads to s3 using amplify storage withmultipart upload"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import { Storage } from 'aws-amplify'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { withAuthenticator } from "aws-amplify-react";
+import config from "./aws-exports";
+Amplify.configure(config);
+
+class App extends React.Component {
+  
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  onChange(e) {
+    const file = e.target.files[0];
+    const filename = e.target.files[0].name;
+    console.log("filename",filename)
+    Storage.put(filename, file, {
+        contentType: 'video/mkv'
+    })
+    .then (result => console.log(result))
+    .catch(err => console.log(err));
 }
 
-export default App;
+render() {
+    return (
+        <input
+            type="file" accept='video/mkv'
+            onChange={(e) => this.onChange(e)}
+        />
+    )
+}
+}
+
+export default withAuthenticator(App, true);
